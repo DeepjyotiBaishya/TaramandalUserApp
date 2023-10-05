33b9d02bb164ea9ad316c16a3ee518df29051d3c
SMIT BORAD <smitborad0@gmail.com>
2023-10-05 13:47:56 +0530

    ..

--- build/rive_common/intermediates/cxx/Debug/6sn1w595/logs/armeabi-v7a/build_command_rive_text.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\DELL\\AppData\\Local\\Android\\sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\DELL\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\rive_common-0.0.3\\android\\.cxx\\Debug\\6sn1w595\\armeabi-v7a" ^
-  rive_text
